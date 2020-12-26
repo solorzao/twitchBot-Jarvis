66e3951a18bd4af41fa603581e8ea7787ecb82a3
solorzao <solorzao@oregonstate.edu>
2020-12-25 20:32:10 -0800

    modified functions to adapt Jarvis to twitchIO API

--- bot.py
+++ bot.py
@@ -45,5 +45,39 @@ async def test(ctx):
     await ctx.send('test passed!')
 
 
+@bot.command(name='roll_dice')
+async def roll(ctx, number_of_dice: int, number_of_sides: int):
+    dice = [
+        str(random.choice(range(1, number_of_sides + 1)))
+        for _ in range(number_of_dice)
+    ]
+    await ctx.send(', '.join(dice))
+
+
+@bot.command(name='roast_me')
+async def roast_initiated(ctx):
+
+    name = ctx.author.name
+
+    roasts = [
+        f'It’s nice to see such a diverse crowd here today. We’ve got Indians, Jews, Whites, and whatever the fuck {name} is.',
+        f'{name} you\'re looking pretty rough this evening. You look like if sweatpants were a person.',
+        f'{name} you\'re my favorite person besides every other person I\'ve ever met.',
+        f'{name} I envy people who have never met you.',
+        f'{name} if you were an inanimate object, you’d be a participation trophy.',
+        f'{name} you are a pizza burn on the roof of the world\'s mouth.',
+        f'{name} if genius skips a generation, your children will be brilliant.',
+        f'{name} you have the charm and charisma of a burning orphanage.',
+        f'{name} if there was a single intelligent thought in your head it would have died from loneliness.',
+        f'{name} I want you to be the pallbearer at my funeral so you can let me down one last time.',
+        f'{name} you are the human embodiment of an eight-dollar haircut.',
+        f'{name} you\'re so inbred you\'re a sandwich.',
+        f'{name} if I had a gun, with two bullets, and I was in a room with Hitler, Bin Laden and you, I would shoot you twice.'
+    ]
+
+    response = random.choice(roasts)
+    await ctx.channel.send(response)
+
+
 if __name__ == "__main__":
     bot.run()
